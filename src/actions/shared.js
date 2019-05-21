8fb6a13948c7ccbb91a707908acd354e93f21fe3
ahmet s <ahmetsametoglu.fr@gmail.com>
2019-05-21 18:13:16 +0200

    feat: action created (handleInitialData)

---
+++ src/actions/shared.js
@@ -0,0 +1,16 @@
+import { getInitialData } from '../utils/api';
+import { receiveUsers } from '../actions/users';
+import { receiveTweets } from '../actions/tweets';
+import { setAuthUser } from '../actions/authedUser';
+
+const AUTHED_ID = 'tylermcginnis';
+
+handleInitialData = () => {
+	return (dispatch) => {
+		return getInitialData().then(({ users, tweets }) => {
+			dispatch(receiveUsers(users));
+			dispatch(receiveTweets(tweets));
+			dispatch(setAuthUser(AUTHED_ID));
+		});
+	};
+};
