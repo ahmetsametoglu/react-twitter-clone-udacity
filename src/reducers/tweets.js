7b34c2721f027f05a6f64612d0742c4dd83cfe08
ahmet s <ahmetsametoglu.fr@gmail.com>
2019-05-21 18:23:15 +0200

    feat: recuder created (tweets)

---
+++ src/reducers/tweets.js
@@ -0,0 +1,11 @@
+import { RECEIVE_TWEETS } from '../actions/tweets';
+
+export const tweets = (state = {}, action) => {
+	switch (action.type) {
+		case RECEIVE_TWEETS:
+			return { ...state, ...action.tweets };
+
+		default:
+			state;
+	}
+};
