9854f4ec15c33fc61e92d9f3b9add96a270a8476
ahmet s <ahmetsametoglu.fr@gmail.com>
2019-05-21 18:29:05 +0200

    feat: reducer created (authedUser)

---
+++ src/reducers/authedUser.js
@@ -0,0 +1,11 @@
+import { SET_AUTH_USER } from '../actions/authedUser';
+
+export const authedUser = (state = null, action) => {
+	switch (action.type) {
+		case SET_AUTH_USER:
+			return { ...state, ...action.id };
+
+		default:
+			return state;
+	}
+};
