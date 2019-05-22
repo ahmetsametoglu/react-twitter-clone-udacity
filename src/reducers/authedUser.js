6f11c2c7869727846a4628c2fe389e30dff1cc47
ahmet s <ahmetsametoglu.fr@gmail.com>
2019-05-22 18:03:20 +0200

    fix: return object format was changed

--- src/reducers/authedUser.js
+++ src/reducers/authedUser.js
@@ -3,7 +3,7 @@ import { SET_AUTH_USER } from '../actions/authedUser';
 export const authedUser = (state = null, action) => {
 	switch (action.type) {
 		case SET_AUTH_USER:
-			return { ...state, ...action.id };
+			return { ...state, id: action.id };
 
 		default:
 			return state;
