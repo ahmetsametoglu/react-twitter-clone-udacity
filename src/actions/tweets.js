f6c0d4a680ca2cfad9ef0a146cae2d2b55a1caa0
ahmet s <ahmetsametoglu.fr@gmail.com>
2019-05-23 18:07:27 +0200

    feat: add_tweet action created

--- src/actions/tweets.js
+++ src/actions/tweets.js
@@ -1,7 +1,9 @@
-import { saveLikeToggle } from '../utils/api';
+import { saveLikeToggle, saveTweet } from '../utils/api';
+import { showLoading, hideLoading } from 'react-redux-loading';
 
 export const RECEIVE_TWEETS = 'RECEIVE_TWEETS';
 export const TOGGLE_TWEET = 'TOGGLE_TWEET ';
+export const ADD_TWEET = 'ADD_TWEET';
 
 export const receiveTweets = (tweets) => {
 	return {
@@ -28,5 +30,29 @@ export function handleToggleTweet(info) {
 			dispatch(toggleTweet(info));
 			alert('The was an error liking the tweet. Try again.');
 		});
+	};
+}
+
+function addTweet(tweet) {
+	return {
+		type: ADD_TWEET,
+		tweet
+	};
+}
+
+export function handleAddTweet(text, replyingTo) {
+	return (dispatch, getState) => {
+		const { authedUser } = getState();
+
+		dispatch(showLoading());
+		console.log(text, replyingTo, authedUser);
+
+		return saveTweet({
+			text,
+			author: authedUser.id,
+			replyingTo
+		})
+			.then((tweet) => dispatch(addTweet(tweet)))
+			.then(() => dispatch(hideLoading()));
 	};
 }
